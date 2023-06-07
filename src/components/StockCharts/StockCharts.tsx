80df99a5db7a1a2b1f77b16c270c24825a4f3d4f
KirillBazavlutskiy <bazik345@gmail.com>
2023-06-07 23:02:17 +0300

    added message if symbols havent been loaded

--- src/components/StockCharts/StockCharts.tsx
+++ src/components/StockCharts/StockCharts.tsx
@@ -1,42 +1,62 @@
-'use client'
+'use client';
 
-import React, {FC, useEffect, useState} from 'react';
-import s from "./StockCharts.module.scss";
-import CandleStickChart from "@/components/CandleStickChart/CandleStickChart";
-import SolidityScreenerService from "@/Services/SolidityScreenerService";
+import React, { FC, useEffect, useState } from 'react';
+import s from './StockCharts.module.scss';
+import CandleStickChart from '@/components/CandleStickChart/CandleStickChart';
+import SolidityScreenerService from '@/Services/SolidityScreenerService';
 
 interface StockChartsProps {
-    activeSymbol: string;
+	activeSymbol: string;
 }
 
 const StockCharts: FC<StockChartsProps> = ({ activeSymbol }) => {
+	const [solitidyPrices, setSolitidyPrices] = useState<number[]>([]);
 
-    const [solitidyPrices, setSolitidyPrices] = useState<number[]>([]);
+	useEffect(() => {
+		if (activeSymbol !== '') {
+			SolidityScreenerService.FindSolidity(activeSymbol, 0.5).then(solidity => {
+				if (solidity !== null) {
+					setSolitidyPrices([
+						solidity.ask?.priceOnMaxVolume || 0,
+						solidity.bid?.priceOnMaxVolume || 0,
+					]);
+				}
+			});
+		}
+	}, [activeSymbol]);
 
-    useEffect(() => {
-        SolidityScreenerService.FindSolidity(activeSymbol, 0.5).then(solidity => {
-            if (solidity !== null) {
-                setSolitidyPrices([solidity.ask?.priceOnMaxVolume || 0, solidity.bid?.priceOnMaxVolume || 0]);
-            }
-        });
-    }, [activeSymbol])
-
-    return (
-        <div className={s.chartsContainer}>
-            <div className={s.chartContainer}>
-                <CandleStickChart symbol={activeSymbol} interval={'5m'} solitydyInfo={solitidyPrices} />
-            </div>
-            <div className={s.chartContainer}>
-                <CandleStickChart symbol={activeSymbol} interval={'30m'} solitydyInfo={solitidyPrices} />
-            </div>
-            <div className={s.chartContainer}>
-                <CandleStickChart symbol={activeSymbol} interval={'2h'} solitydyInfo={solitidyPrices} />
-            </div>
-            <div className={s.chartContainer}>
-                <CandleStickChart symbol={activeSymbol} interval={'4h'} solitydyInfo={solitidyPrices} />
-            </div>
-        </div>
-    );
+	return (
+		<div className={s.chartsContainer}>
+			<div className={s.chartContainer}>
+				<CandleStickChart
+					symbol={activeSymbol}
+					interval={'5m'}
+					solitydyInfo={solitidyPrices}
+				/>
+			</div>
+			<div className={s.chartContainer}>
+				<CandleStickChart
+					symbol={activeSymbol}
+					interval={'30m'}
+					solitydyInfo={solitidyPrices}
+				/>
+			</div>
+			<div className={s.chartContainer}>
+				<CandleStickChart
+					symbol={activeSymbol}
+					interval={'2h'}
+					solitydyInfo={solitidyPrices}
+				/>
+			</div>
+			<div className={s.chartContainer}>
+				<CandleStickChart
+					symbol={activeSymbol}
+					interval={'4h'}
+					solitydyInfo={solitidyPrices}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default StockCharts;
