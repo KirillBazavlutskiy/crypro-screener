44eb3b24cef7d6ca1d4182eb3929b99c7e1c4bc6
KirillBazavlutskiy <bazik345@gmail.com>
2023-06-08 11:27:20 +0300

    added serverideprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import SolidityScreenerService from "@/Services/SolidityScreenerService";
 import s from './index.module.scss';
 import StockCharts from "@/components/StockCharts/StockCharts";
 import {useState} from "react";
-import {GetStaticProps} from "next";
+import {GetServerSideProps} from "next";
 
 interface IndexPageProps {
     symbols: string[];
@@ -35,7 +35,7 @@ export default function Index({ symbols, error }: IndexPageProps) {
     )
 }
 
-export const getStaticProps: GetStaticProps<IndexPageProps> = async () => {
+export const getServerSideProps: GetServerSideProps<IndexPageProps> = async () => {
     try {
         const symbols = await SolidityScreenerService.FindAllSolidity();
         return {
