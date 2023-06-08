2e04385cad9b7fd8ab8ca5848dae03095a686492
KirillBazavlutskiy <bazik345@gmail.com>
2023-06-08 11:19:40 +0300

    added error handler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,36 +5,50 @@ import {useState} from "react";
 import {GetStaticProps} from "next";
 
 interface IndexPageProps {
-  symbols: string[];
+    symbols: string[];
+    error: any | null;
 }
 
-export default function Index({ symbols }: IndexPageProps) {
+export default function Index({ symbols, error }: IndexPageProps) {
 
-  const [activeSymbol, setActiveSymbol] = useState<string>("");
+    const [activeSymbol, setActiveSymbol] = useState<string>("");
 
-  return (
-      <div className={s.container}>
-        <div className={s.navbar}>
-          {
-            symbols.map(symbol =>
-                <button
-                    key={symbol}
-                    onClick={() => setActiveSymbol(symbol)}
-                    className={activeSymbol === symbol ? s.activeSymbol : ''}
-                >{symbol}</button>
-            )
-          }
+    console.log(symbols);
+    console.log(error);
+
+    return (
+        <div className={s.container}>
+            <div className={s.navbar}>
+                {
+                    symbols.map(symbol =>
+                        <button
+                            key={symbol}
+                            onClick={() => setActiveSymbol(symbol)}
+                            className={activeSymbol === symbol ? s.activeSymbol : ''}
+                        >{symbol}</button>
+                    )
+                }
+            </div>
+
+            <StockCharts activeSymbol={activeSymbol} />
         </div>
-
-        <StockCharts activeSymbol={activeSymbol} />
-      </div>
-  )
+    )
 }
 
 export const getStaticProps: GetStaticProps<IndexPageProps> = async () => {
-  const symbols = await SolidityScreenerService.FindAllSolidity();
-  return {
-    props: { symbols },
-    revalidate: 300,
-  }
+    try {
+        const symbols = await SolidityScreenerService.FindAllSolidity();
+        return {
+            props: { symbols, error: null },
+            revalidate: 300,
+        }
+    } catch (e) {
+        return {
+            props: {
+                symbols: [ 'error' ],
+                error: e
+            },
+            revalidate: 300,
+        }
+    }
 }
